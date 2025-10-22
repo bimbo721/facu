e6eece5b3ebc35e37ee03dbb960805673245a423
bimbo721 <joacoquinteros83@gmail.com>
2025-10-22 15:09:13 -0300

    a

--- TP FINAL/modulostp3.py
+++ TP FINAL/modulostp3.py
@@ -1,8 +1,10 @@
+import os.path
 import pickle
+from clase import *
 
 
 def validar(op):
-    permitidos = ['0', '1', '2']
+    permitidos = ['0', '1', '2', '3', '4']
     while True:
         op = op.strip()
         if len(op) == 1 and op in permitidos:
@@ -10,74 +12,38 @@ def validar(op):
         op = input('Error: se pidio que cargue un numero entre 0 y 2: ')
 
 
-def cargar_envio(linea):
-    linea = linea.strip()
-    if linea == "":
-        return None
-
-    indice = linea.split(',')
-
-    if len(indice) < 7:
-        indice = linea.split(';')
-        if len(indice) < 7:
-            return None
-
-
-    codigo = indice[0].strip()
-
-
-    cod_ord_pago = codigo.split('|')
-    if len(cod_ord_pago) != 3:
-        return None
-
-    cod_mon_origen = int(cod_ord_pago[0])
-    cod_mon_pago = int(cod_ord_pago[1])
-    id_pago = cod_ord_pago[2].strip()
-
-    id_dest = indice[1].strip()
-    nom_dest = indice[2].strip()
-    tasa_conv = float(indice[3])
-    monto_nominal = float(indice[4])
-    alg_com = int(indice[5])
-    alg_imp = int(indice[6])
-
-    objeto = Envio(cod_mon_origen, cod_mon_pago, id_pago, id_dest, nom_dest, tasa_conv, monto_nominal, alg_com, alg_imp)
-
-    return objeto
-
-
+# -------------------------------r1------------------------------
 def procesar_archivo_csv(v):
     m = open("envios.csv", "rt")
-    distintos = 0
 
     for linea in m:
-        obj = cargar_envio(linea)
-
-        if obj is not None:
-            acomodar_vector(v,obj)
-            if obj.cod_mon_origen != obj.cod_mon_pago:
-                distintos = distintos + 1
+        obj = generar_envio(linea)
+        inserccion_ordenada(v, obj)
 
     m.close()
-    return distintos
 
-##################
-def acomodar_vector(v,obj):
-    izq = 0
-    der = len(v)
 
-    while izq < der:
-        centro = (izq + der) // 2
-        if v[centro].id_dest >= obj.id_dest:
-            izq = centro + 1
+def inserccion_ordenada(v, o):
+    n = len(v)
+    pos = n
+    izq, der = 0, n - 1
+    while izq <= der:
+        c = (izq + der) // 2
+        if v[c].identificacion_destinatario == o.identificacion_destinatario:
+            pos = c
+            break
+        if o.identificacion_destinatario > v[c].identificacion_destinatario:
+            der = c - 1
         else:
-            der = centro
+            izq = c + 1
 
-    pos = izq
-    v[pos:pos] = [obj]
+    if izq > der:
+        pos = izq
+    v[pos:pos] = [o]
+
 
 ##################
-def buscar_valor(v,i):
+def buscar_valor(v, i):
     n = len(v)
     nuevo_indice = 0
 
@@ -85,168 +51,33 @@ def buscar_valor(v,i):
         print("indice fuera de rango")
         return
 
+    print("r1.1:", v[i].obtener_identificador_pago())
 
-    for f in range(n):
-        if i == f:
-            print("r1.1: ",v[f].id_pago)
+    if i % 2 == 0:
+        nuevo_indice = i // 2
 
-            if i % 2 == 0:
-                nuevo_indice = i // 2
+    elif i % 2 != 0:
+        nuevo_indice = 3 * i + 1
 
-            elif i % 2 != 0:
-                nuevo_indice = 3 * i + 1
+    if nuevo_indice >= n:  # mostrar ultimo
+        nuevo_indice = n - 1
 
-            if nuevo_indice >= n: #mostrar ultimo
-                nuevo_indice = n - 1
-
-            print("r1.2: ",v[nuevo_indice].id_pago)
-            break
+    print("r1.2:", v[nuevo_indice].obtener_identificador_pago())
 
 
-def generar_nuevo_archivo(v,m_comisiones):
-    n = len(v)
-
-    matriz_acumulador = []
-    #filas son las 5 monedas
-    #columna 0 indica de que moneda es de origen
-    #columa 1 suma_comisiones
-    #columa 2 cantidad_objetos
-
-
-    for i in range(n):
-        objeto = v[i]
-        comision = m_comisiones[i][0] #columna de las comisiones
-        moneda_origen = objeto.obtener_codigo_moneda_origen()
-
-
-        #esto verifica que existe la moneda en la matriz acu,si no,la agrega
-        pos = -1
-        for fila in range(len(matriz_acumulador)):
-            if matriz_acumulador[fila][0] == moneda_origen:
-                pos = fila
-                break
-
-        if pos == -1:
-            matriz_acumulador.append([moneda_origen,comision,1])
-        else:
-            matriz_acumulador[pos][1] += comision
-            matriz_acumulador[pos][2] += 1
-
-
-    promedio = [0.0] * 6
-    for k in range(len(matriz_acumulador)):
-        moneda = matriz_acumulador[k][0]
-        suma_comision = matriz_acumulador[k][1]
-        cant_objetos = matriz_acumulador[k][2]
-
-        if cant_objetos != 0: #poner promedio[moneda] hace que nunca entre al casillero
-            #0 del promedio ya que "moneda = matriz_acumulador[k][0]" se guardo la moneda
-            # en la matriz como (1,2,3,4 o 5)
-            promedio[moneda] = suma_comision / cant_objetos
-
-
-    #en la m_comisiones es asi:
-    # las filas es un envio,o sea un objeto del vector
-    # las columa 0 es comision
-    # las columna 1 es el monto base
-
-
-    #dato importante para entender,nunca se usan las filas de la matriz_acumulador,ya que
-    #el valor/tipo de las monedas estan en la columna 0
-    m = open("archivocomision", "wb")
-    for i in range(n):
-        comision = m_comisiones[i][0]
-        moneda = v[i].obtener_codigo_moneda_origen()
-
-        if comision > promedio[moneda]:
-            pickle.dump(v[i],m)
-
-    m.close()
-
-
-
-#r22
-def identificador(m_2,v,matriz):
-    v_acu = []
-    for fila in range(len(matriz)):
-        comision = matriz[fila][0]
-        monto_base = matriz[fila][1]
-        impuesto = m_2[fila][0]
-
-        nominal = comision + monto_base
-        if nominal != 0:
-            porc_desc = (impuesto + comision) * 100 / nominal
-        else:
-            porc_desc = 0
-        v_acu.append(porc_desc)
-
-    n = len(v_acu)
-    for i in range(n-1):
-        for j in range(i+1,n):
-            if v_acu[i] < v_acu[j]:
-                v[i] , v[j] = v[j] , v[i]
-                v_acu[i], v_acu[j] = v_acu[j], v_acu[i]
-                m_2[i] , m_2[j] = m_2[j], m_2[i]
-                matriz[i] , matriz[j] = matriz[j] , matriz[i]
-
-
-    iden_mayor_porc_desc = v[0].id_pago
-
-    return iden_mayor_porc_desc
-
-
-#r22
-def calculo_impositivo(v,matriz):
-    impuesto = 0
-    monto_final = 0
-    m_2 = []
-
-    for i in range(len(matriz)):
-        mont_ba = matriz[i][1]
-
-        i = v[i]
-        if i.alg_imp == 1:
-            if mont_ba <= 300000:
-                impuesto = 0
-            else:
-                excedente = mont_ba - 300000
-                impuesto = excedente * 25 / 100
-
-            monto_final = mont_ba - impuesto
-
-        elif i.alg_imp == 2:
-            if mont_ba < 50000:
-                impuesto = 50
-            else:
-                impuesto = 100
-            monto_final = mont_ba - impuesto
-
-        elif i.alg_imp == 3:
-            impuesto = mont_ba * 3 / 100
-            monto_final = mont_ba - impuesto
-
-        m_2.append([impuesto,monto_final])
-
-    return m_2
-
-#r21
+# -------------------------r2------------------------------
 def comisiones(vector):
     matriz_comi = []
-
-    #las filas es un envio,o sea un objeto del vector
-    #las columa 0 es comision
-    #las columna 1 es el monto base
-
 
     for i in vector:
         monto_n = i.monto_nominal
         comision = 0
 
-        if i.alg_com == 1:
+        if i.algoritmo_comision == 1:
             comision = 9 * monto_n / 100
             monto_base = monto_n - comision
 
-        elif i.alg_com == 2:
+        elif i.algoritmo_comision == 2:
             if monto_n < 50000:
                 comision = 0
             elif monto_n < 80000:
@@ -255,20 +86,20 @@ def comisiones(vector):
                 comision = 7.8 * monto_n / 100
             monto_base = monto_n - comision
 
-        elif i.alg_com == 3:
+        elif i.algoritmo_comision == 3:
             if monto_n > 25000:
                 comision = 6 * monto_n / 100
             comision += 100
             monto_base = monto_n - comision
 
-        elif i.alg_com == 4:
+        elif i.algoritmo_comision == 4:
             if monto_n <= 100000:
                 comision = 500
             else:
                 comision = 1000
             monto_base = monto_n - comision
 
-        elif i.alg_com == 5:
+        elif i.algoritmo_comision == 5:
             if monto_n < 500000:
                 comision = 0
             else:
@@ -285,103 +116,183 @@ def comisiones(vector):
 
     return matriz_comi
 
-#r21
-def porcentaje_comision(matriz):
-    suma_nominal = 0
-    suma_comisiones = 0
 
-    for fila in matriz:
-        comi = fila[0]
-        base = fila[1]
+def generar_nuevo_archivo(v, m_comisiones, arc):
+    n = len(v)
 
-        nominal = comi + base
+    matriz_acumulador = []
 
-        suma_nominal += nominal
-        suma_comisiones += comi
-
-    if suma_nominal != 0:
-        porc_comi = suma_comisiones * 100 / suma_nominal
-    else:
-        return 0
-
-    return porc_comi
-
-def sumatoria_impuestos_por_par_monedas(v, m_2):
-    resultado = []
-
-    i = 0
-    while i < len(v):
-        envio = v[i]
-        impuesto = m_2[i][0]
-        mo = envio.cod_mon_origen
-        mp = envio.cod_mon_pago
+    for i in range(n):
+        objeto = v[i]
+        comision = m_comisiones[i][0]  # columna de las comisiones
+        moneda_origen = objeto.obtener_codigo_moneda_origen()
 
         pos = -1
-        j = 0
-        while j < len(resultado):
-            if resultado[j][0] == mo and resultado[j][1] == mp:
-                pos = j
+        for fila in range(len(matriz_acumulador)):
+            if matriz_acumulador[fila][0] == moneda_origen:
+                pos = fila
                 break
-            j = j + 1
 
         if pos == -1:
-            resultado.append([mo, mp, float(impuesto)])
+            matriz_acumulador.append([moneda_origen, comision, 1])
         else:
-            resultado[pos][2] = resultado[pos][2] + float(impuesto)
+            matriz_acumulador[pos][1] += comision
+            matriz_acumulador[pos][2] += 1
 
-        i = i + 1
+    promedio = [0.0] * 6
+    for k in range(len(matriz_acumulador)):
+        moneda = matriz_acumulador[k][0]
+        suma_comision = matriz_acumulador[k][1]
+        cant_objetos = matriz_acumulador[k][2]
 
-    return resultado
+        if cant_objetos != 0:
+            promedio[moneda] = suma_comision / cant_objetos
 
+    m = open(arc, "wb")
+    for i in range(n):
+        comision = m_comisiones[i][0]
+        moneda = v[i].obtener_codigo_moneda_origen()
 
-class Envio:
-    def __init__(self, codigo, identificacion_destinatario,
-                 nombre_destinatario, tasa, monto_nominal,
-                 algoritmo_comision, algoritmo_impositivo):
-        self.codigo = codigo
-        self.identificacion_destinatario = identificacion_destinatario
-        self.nombre_destinatario = nombre_destinatario
-        self.tasa = tasa
-        self.monto_nominal = monto_nominal
-        self.algoritmo_comision = algoritmo_comision
-        self.algoritmo_impositivo = algoritmo_impositivo
+        if comision > promedio[moneda]:
+            pickle.dump(v[i], m)
 
-    def obtener_identificador_pago(self):
-        identificador_pago = self.codigo.split("|")
-        return identificador_pago[2]
-
-    def obtener_codigo_moneda_origen(self):
-        moneda_origen = self.codigo.split("|")
-        return int(moneda_origen[0])
-
-    def obtener_codigo_moneda_destino(self):
-        moneda_destino = self.codigo.split("|")
-        return int(moneda_destino[1])
-
-    def __str__(self):
-        return (
-            f"Identificador de pago: {self.obtener_identificador_pago()} - Identificación: {self.identificacion_destinatario} -"
-            f" Nombre: {self.nombre_destinatario} - Tasa: {self.tasa} - Monto nominal: {self.monto_nominal}")
+    m.close()
 
 
-def generar_envio(token):
-    datos_base = token[:-1]
-    datos_base = datos_base.split(",")
-    codigo = datos_base[0]
-    identificacion_destinatario = datos_base[1]
-    nombre_destinatario = datos_base[2]
-    tasa = float(datos_base[3])
-    monto_nominal = int(datos_base[4])
-    algoritmo_comision = int(datos_base[5])
-    algoritmo_impositivo = int(datos_base[6])
-    return Envio(codigo, identificacion_destinatario, nombre_destinatario, tasa, monto_nominal, algoritmo_comision,
-                 algoritmo_impositivo)
+# r21
+def mostrar_archivo(arc):
+    m = open(arc, 'rb')
+
+    tam = os.path.getsize(arc)
+    while m.tell() < tam:
+        obj = pickle.load(m)
+        print(obj)
+
+    m.close()
 
 
-if __name__ == "__main__":
-    envio_base = "05|04|4616A0743D75FC8C,AF188371E36A,Shansa B. Alexis,1,11374056,4,2\n"
-    envio = generar_envio(envio_base)
-    print(envio)
-    print(envio.obtener_identificador_pago())
-    print(envio.obtener_codigo_moneda_origen())
-    print(envio.obtener_codigo_moneda_destino())
+# r22
+def mostrar_envios_mayorcomision(arc):
+    m = open(arc, 'rb')
+    tam = os.path.getsize(arc)
+    envios = []
+    while m.tell() < tam:
+        obj = pickle.load(m)
+        envios.append(obj)
+    m.close()
+
+    matriz_comi = comisiones(envios)  # [comision, monto_base]
+
+    if not envios:
+        print("El archivo no contiene envíos.")
+        return
+
+    ordenar_por_comision_descendente(envios, matriz_comi)
+
+    n = len(envios)
+    if n < 3:
+        for i in range(n):
+            print(envios[i])
+    else:
+        for i in range(3):
+            print(envios[i])
+
+
+def ordenar_por_comision_descendente(envios, matriz_comi):
+    # metodo de la burbuja
+    n = len(envios)
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            comision_actual = matriz_comi[j][0]
+            comision_siguiente = matriz_comi[j + 1][0]
+
+            if comision_actual < comision_siguiente:
+                matriz_comi[j], matriz_comi[j + 1] = matriz_comi[j + 1], matriz_comi[j]
+
+                envios[j], envios[j + 1] = envios[j + 1], envios[j]
+
+
+# -------------------------r3-----------------------------
+# r31
+def redondear_centena(monto):
+    return int(round(monto / 100.0)) * 100
+
+
+# r32
+def buscar_modificar_por_id(vector, id_dest):
+    for envio in vector:
+        if envio.identificacion_destinatario == id_dest:
+            antes = envio.monto_nominal
+            despues = redondear_centena(antes * 1.17)
+            envio.monto_nominal = despues
+            return antes, despues
+    return 0, 0
+
+
+# -----------------------------r4-------------------------------
+def calculo_impositivo(v, matriz):
+    impuesto = 0
+    monto_final = 0
+    m_2 = []
+
+    for i in range(len(matriz)):
+        mont_ba = matriz[i][1]
+
+        i = v[i]
+        if i.algoritmo_impositivo == 1:
+            if mont_ba <= 300000:
+                impuesto = 0
+            else:
+                excedente = mont_ba - 300000
+                impuesto = excedente * 25 / 100
+
+            monto_final = mont_ba - impuesto
+
+        elif i.algoritmo_impositivo == 2:
+            if mont_ba < 50000:
+                impuesto = 50
+            else:
+                impuesto = 100
+            monto_final = mont_ba - impuesto
+
+        elif i.algoritmo_impositivo == 3:
+            impuesto = mont_ba * 3 / 100
+            monto_final = mont_ba - impuesto
+
+        m_2.append([impuesto, monto_final])
+
+    return m_2
+
+
+def generar_matriz_maximos(v, m_montos_finales):
+    n = len(v)
+    filas = 6
+    columnas = 6
+    matriz_max = [[None] * columnas for i in range(filas)]
+
+    matriz_montos = [[-1.0] * columnas for _ in range(filas)]
+
+    for i in range(n):
+        envio = v[i]
+
+        mon_origen = envio.obtener_codigo_moneda_origen()
+        mon_pago = envio.obtener_codigo_moneda_destino()
+
+        monto_final = m_montos_finales[i][1]
+
+        if monto_final > matriz_montos[mon_origen][mon_pago]:
+            matriz_montos[mon_origen][mon_pago] = monto_final
+
+            matriz_max[mon_origen][mon_pago] = envio
+
+    return matriz_max
+
+
+# r41
+def mostrar_codigo_op4(matriz):
+    for mo in range(1, 6):
+        for mp in range(1, 6):
+            envio_max = matriz[mo][mp]
+            if envio_max is not None:
+                print(f'{mo}|{mp}|{envio_max.obtener_identificador_pago()}')
+
--- TP FINAL/tp3.py
+++ TP FINAL/tp3.py
@@ -1,8 +1,11 @@
 from modulostp3 import *
 
+
 def principal():
     op = -1
     vector = []
+    m_comisiones = []
+    archivo = 'archivocomision.dat'
     while op != 0:
         print("Opciones:")
         print("1)cargar envios")
@@ -13,70 +16,50 @@ def principal():
         op = validar(input("Ingrese opción: "))
         op = int(op)
 
-
         if op == 1:
-            distintos = procesar_archivo_csv(vector)
-            r11 = len(vector)
-            r12 = distintos
-#############
+            procesar_archivo_csv(vector)
+
             i = int(input("ingrese un valor i: "))
             buscar_valor(vector,i)
-
 
         if op == 2:
             if not vector:
                 print("error,primero declare el vector")
             else:
-                m_comisiones = comisiones(vector)
-                r21 = porcentaje_comision(m_comisiones)
-                round(r21,2)
+                if not m_comisiones:
+                    m_comisiones = comisiones(vector)
 
-                #r22
-                matriz_2 = calculo_impositivo(vector,m_comisiones)
-                r22 = identificador(matriz_2,vector,m_comisiones)
+                generar_nuevo_archivo(vector,m_comisiones,archivo)
 
+                #r2.1
+                print('r2.1:')
+                mostrar_archivo(archivo)
+                print()
+                #r2.2
+                print("r2.2:")
+                mostrar_envios_mayorcomision(archivo)
 
-                #r23
-                impuesto_22 = matriz_2[0][0]
-                monto_base_22 = m_comisiones[0][1]
+        if op == 3:
+            if not vector:
+                print("error,primero declare el vector")
+            else:
+                id_dest = input("Ingrese la identificación del destinatario a buscar: ")
+                r31, r32 = buscar_modificar_por_id(vector, id_dest)
+                print("r3.1:", r31)
+                print("r3.2:", r32)
 
-                monto_final_orden_22 = monto_base_22 - impuesto_22
+        if op == 4:
+            if not vector and not os.path.exists(archivo):
+                print("error,primero declare el vector")
+            else:
+                if not m_comisiones:
+                    m_comisiones = comisiones(vector)
 
-                tasa = vector[0].tasa_conv
-                r23_final = monto_final_orden_22 * tasa
+                m_impuestos_mfinal = calculo_impositivo(vector, m_comisiones)
+                m_max_mfinal = generar_matriz_maximos(vector,m_impuestos_mfinal)
 
-                print("r2.1:", r21)
-                print("r2.2:", r22)
-                print("r2.3:", r23_final)
-
-                ## extra
-
-                resultado = sumatoria_impuestos_por_par_monedas(vector, matriz_2)
-                n = len(resultado)
-                i = 0
-                while i < n - 1:
-                    j = i + 1
-                    while j < n:
-                        if (resultado[i][0] > resultado[j][0]) or (resultado[i][0] == resultado[j][0] and resultado[i][1] > resultado[j][1]):
-                            aux = resultado[i]
-                            resultado[i] = resultado[j]
-                            resultado[j] = aux
-                        j = j + 1
-                    i = i + 1
-
-
-                generar_nuevo_archivo(vector)
-
-
-                print("r2.4:")
-                k = 0
-                while k < len(resultado):
-                    mo = resultado[k][0]
-                    mp = resultado[k][1]
-                    total = resultado[k][2]
-                    print("moneda_origen=" + str(mo) + ", moneda_pago=" + str(mp) + ":", round(total, 2))
-                    k = k + 1
-
+                print('r4.1:')
+                mostrar_codigo_op4(m_max_mfinal)
 
         if op == 0:
             print("el programa terminó")
